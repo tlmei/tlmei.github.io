0358797cd864fb5cba768783122fd76453d94c6f
Troy Mei <tm2903@nyu.edu>
2020-11-01 12:49:11 -0500

    Updates

---
+++ precache-manifest.df0bfc0566031956485ecea916650a71.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f159767257750fbc722e78d31694d79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3780542e13acc5f283fc",
+    "url": "/static/css/main.7a0e4ae9.chunk.css"
+  },
+  {
+    "revision": "61e1c33bbd66349b4eb5",
+    "url": "/static/js/2.6419b587.chunk.js"
+  },
+  {
+    "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
+    "url": "/static/js/2.6419b587.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3780542e13acc5f283fc",
+    "url": "/static/js/main.16586a53.chunk.js"
+  },
+  {
+    "revision": "9b41c7e13f690904baac",
+    "url": "/static/js/runtime-main.c3e8af0c.js"
+  },
+  {
+    "revision": "8756d0c1de8851215fc7fdee66d91336",
+    "url": "/static/media/avatar.8756d0c1.jpg"
+  },
+  {
+    "revision": "7916100208141cead19c35374cb63c8b",
+    "url": "/static/media/stylemescreen.79161002.png"
+  }
+]);

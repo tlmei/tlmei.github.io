7c7261761ff8aa4d3a329ef135ea4b21c4869945
Troy Mei <tm2903@nyu.edu>
2020-11-01 11:30:38 -0500

    Updates

---
+++ precache-manifest.5626a66d79fb725f1f446b9170c83993.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ffcfa1c7965abf152854507d1d005b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57d1364b61eef4efeace",
+    "url": "/static/css/main.dafcbc57.chunk.css"
+  },
+  {
+    "revision": "5e9545d50892bf83f212",
+    "url": "/static/js/2.2e9c93c7.chunk.js"
+  },
+  {
+    "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
+    "url": "/static/js/2.2e9c93c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57d1364b61eef4efeace",
+    "url": "/static/js/main.81d7d6aa.chunk.js"
+  },
+  {
+    "revision": "9b41c7e13f690904baac",
+    "url": "/static/js/runtime-main.c3e8af0c.js"
+  },
+  {
+    "revision": "8756d0c1de8851215fc7fdee66d91336",
+    "url": "/static/media/avatar.8756d0c1.jpg"
+  }
+]);
